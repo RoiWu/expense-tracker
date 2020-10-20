88fca042ff57b9cc12372dfb44a4cc11ba89c260
RoiWu <roih1h116@gmail.com>
2020-10-20 22:17:26 +0800

    filter done

--- routes/modules/filter.js
+++ routes/modules/filter.js
@@ -3,25 +3,23 @@ const router = express.Router()
 
 const Record = require('../../models/Record')
 
-router.get('/:name', (req, res) => {
-  const { name } = req.params
-  console.log("name=",name)
-  if (name === "all") {
+router.get('/:category', (req, res) => {
+  const { category } = req.params
+  console.log("category=", category)
+  if (category === "all") {
     res.redirect("/")
   } else {
-    Record.find({ name: {$eq:`${name}`} })
+    Record.find({ category: category })
       .lean()
       .then(todolists => {
-        console.log("result=",todolists)
         let sum = 0
         for (let todo of todolists) {
           sum += todo.amount
         }
-        const category = name
         res.render('index', { sum, todolists, category })
       }) // 將資料傳給 index 樣板
       .catch(error => console.error(error)) // 錯誤處理
-    console.log(req.params)    
+    console.log(req.params)
   }
 
 })
